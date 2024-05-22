57cfdc935d1fbb1c11f8820b2c8c54b562ffc24e
rafaelarrocha <145746078+rafaelarrocha@users.noreply.github.com>
2024-05-22 02:07:41 -0300

    Add files via upload

---
+++ profile/app/Filament/Resources/BrandResource/Pages/EditBrand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\BrandResource\Pages;
+
+use App\Filament\Resources\BrandResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditBrand extends EditRecord
+{
+    protected static string $resource = BrandResource::class;
+
+    protected function getRedirectUrl(): ?string
+
+    {
+    
+        return static::getResource()::getUrl('index');
+    
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
